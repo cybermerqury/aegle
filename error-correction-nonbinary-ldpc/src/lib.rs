993ca688db126b293cd12ca81e69eaeb5f7374c9
Noel Farrugia <noel@merqury.eu>
2026-03-10 11:23:51 +0100

    Remove link to README page that is no longer valid

--- error-correction-nonbinary-ldpc/src/lib.rs
+++ error-correction-nonbinary-ldpc/src/lib.rs
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: PolyForm-Noncommercial-1.0.0
 // SPDX-FileCopyrightText:  © 2024 - 2026 Merqury Cybersecurity Ltd <info@merqury.eu>
 
-#![doc = include_str!("../README.md")]
-
 pub mod decoder;
 pub mod finite_field;
 pub mod polynomial;
