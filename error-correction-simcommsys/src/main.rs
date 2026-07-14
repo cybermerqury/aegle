78e2c601579b386cf95b9785ab352d3233ee21fe
Zachary <zach.cauchi@merqury.eu>
2026-07-14 10:54:05 +0200

    MQY-1871: Create base simcommsys integration crate

---
+++ error-correction-simcommsys/src/main.rs
@@ -0,0 +1,39 @@
+use core::{
+    key_state_machine::{Key, Reconciled, Reconciling},
+    traits::{
+        FollowerRequests, FollowerResponse, PPError, PPStep, PostProcessingSetup,
+        PostProcessingStep,
+    },
+};
+
+pub struct SimCommSys;
+
+impl PostProcessingSetup for SimCommSys {
+    type Worker = SimCommSys;
+    type InitialStage = Reconciling;
+    type SetupArgs = f64;
+
+    fn setup(self, _: Key<Self::InitialStage>, _: Self::SetupArgs) -> Self::Worker {
+        Self {}
+    }
+}
+
+impl PostProcessingStep for SimCommSys {
+    type InitialStage = Reconciling;
+    type FinalStage = Reconciled;
+    type Result = ();
+
+    fn step(&mut self) -> Result<PPStep<Self::Result, FollowerRequests>, PPError> {
+        unimplemented!("SimCommSys processing step to be implemented.");
+    }
+
+    fn update(&mut self, _: FollowerResponse) -> Result<(), PPError> {
+        unimplemented!("SimCommSys processing update to be implemented.");
+    }
+
+    fn finalize(self) -> Result<Key<Self::FinalStage>, PPError> {
+        unimplemented!("SimCommSys finalization to be implemented.");
+    }
+}
+
+fn main() {}
