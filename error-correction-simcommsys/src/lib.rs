6f2847162869d5985d85eca164688e25beb74f10
Zachary <zach.cauchi@merqury.eu>
2026-07-15 13:58:29 +0200

    MQY-1871: Add make targets to examples dir

--- error-correction-simcommsys/src/lib.rs
+++ error-correction-simcommsys/src/lib.rs
@@ -32,14 +32,18 @@ impl PostProcessingStep for SimCommSys {
     type Result = ();
 
     fn step(&mut self) -> Result<PPStep<Self::Result, FollowerRequests>, PPError> {
-        unimplemented!("SimCommSys processing step to be implemented.");
+        Err(PPError::new(
+            "SimCommSys processing step to be implemented.",
+        ))
     }
 
     fn update(&mut self, _: FollowerResponse) -> Result<(), PPError> {
-        unimplemented!("SimCommSys processing update to be implemented.");
+        Err(PPError::new(
+            "SimCommSys processing update to be implemented.",
+        ))
     }
 
     fn finalize(self) -> Result<Key<Self::FinalStage>, PPError> {
-        unimplemented!("SimCommSys finalization to be implemented.");
+        Err(PPError::new("SimCommSys finalization to be implemented."))
     }
 }
